039effaafa00e64989879df997b35c4851742e4b
LaurenceHealy <109047066+LaurenceHealy@users.noreply.github.com>
2022-08-19 13:19:25 -0400

    Initial commit

---
+++ Assignment5.3/Views/UserControl1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Assignment5._3.Views
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        
+        public event EventHandler LogonSuccess;
+        public event EventHandler LogonFailure;
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            var user = userName.Text;
+            var pass = password.Password;
+            if((user == "Teacher") && (pass == "Admin"))
+            {
+                LogonSuccess(this, null);
+                return;
+            }
+            else
+            {
+                LogonFailure(this, null);
+            }
+            
+            
+            
+
+        }
+    }
+}
